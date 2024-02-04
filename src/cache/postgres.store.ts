8edb1ed3aada408e158f3321a244372b76d2e6b3
Luciano Santos <luciano.ssants@gmail.com>
2024-02-04 14:47:30 -0300

    feat(cache): add cache via postgres

---
+++ src/cache/postgres.store.ts
@@ -0,0 +1,80 @@
+import { CacheStore } from '@nestjs/cache-manager';
+import * as Pool from 'pg-pool';
+import { Client } from 'pg';
+import * as moment from 'moment-timezone';
+
+export interface PostgresStoreOptions {
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  database: string;
+  poolSize: number;
+  sslMode: string;
+}
+
+export class PostgresStore implements CacheStore {
+  private pool: Pool<Client>;
+
+  constructor(private readonly options: PostgresStoreOptions) {
+    this.pool = new Pool({
+      host: options.host,
+      port: options.port,
+      user: options.user,
+      password: options.password,
+      database: options.database,
+      max: options.poolSize,
+    });
+  }
+
+  async set<T>(key: string, value: T, options?: number): Promise<void> {
+    await this.deleteExpired();
+
+    const expiresAt = moment.tz('UTC').add(options, 'milliseconds');
+
+    const insertQuery = `INSERT INTO cache (key, value, "expiresAt")
+                         VALUES ($1, $2, $3)`;
+
+    const json = JSON.stringify(value);
+
+    await this.pool.query(insertQuery, [key, json, expiresAt.toISOString()]);
+  }
+
+  async get<T>(key: string): Promise<T | undefined> {
+    await this.deleteExpired();
+
+    const query = `SELECT value, "expiresAt"
+                   FROM cache
+                   WHERE key = $1`;
+
+    const result = await this.pool.query(query, [key]);
+
+    if (result.rows.length === 0) {
+      return undefined;
+    }
+
+    const { value, expiresAt } = result.rows[0];
+
+    if (moment.tz(expiresAt, 'UTC').isBefore(moment.tz('UTC'))) {
+      return undefined;
+    }
+
+    return value as T;
+  }
+
+  async del?(key: string): Promise<void> {
+    await this.deleteExpired();
+
+    const query = 'DELETE FROM cache WHERE key = $1';
+
+    await this.pool.query(query, [key]);
+  }
+
+  private async deleteExpired(): Promise<void> {
+    const query = `DELETE
+                   FROM cache
+                   WHERE "expiresAt" < $1`;
+
+    await this.pool.query(query, [moment.tz('UTC').toISOString()]);
+  }
+}
