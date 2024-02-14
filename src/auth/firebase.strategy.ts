b8d1e452d3879a3b4d98fc18768b338f30a5d4d1
Luciano Santos <luciano.ssants@gmail.com>
2024-02-14 09:49:42 -0300

    fix(auth): update token cookie key (#19)

--- src/auth/firebase.strategy.ts
+++ src/auth/firebase.strategy.ts
@@ -12,8 +12,8 @@ import { ExtractJwt, Strategy } from 'passport-firebase-jwt';
 function extractJWTFromCookie(request: Request): string | null {
   const cookies = request.cookies;
 
-  if (cookies && cookies.accessToken) {
-    return cookies.accessToken;
+  if (cookies && cookies['_Secure-access-token']) {
+    return cookies['_Secure-access-token'];
   }
 
   return null;
