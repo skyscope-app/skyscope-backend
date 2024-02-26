d802195953818111a06d704064b61b6d1b9a7603
Luciano Santos <luciano.ssants@gmail.com>
2024-02-25 22:22:52 -0300

    ci: add firebae deploy

--- functions/network-fetch/index.ts
+++ functions/network-fetch/index.ts
@@ -3,17 +3,22 @@ import { validateConfiguration } from '@/configurations/configuration';
 import { IVAOService } from '@/networks/services/ivao.service';
 import { VATSIMService } from '@/networks/services/vatsim.service';
 import { NestFactory } from '@nestjs/core';
-import { pubsub } from 'firebase-functions';
+import { runWith } from 'firebase-functions';
 import { Configuration } from 'functions/network-fetch/configuration';
 
-export const networksFetcher = pubsub.schedule('* * * * *').onRun(async () => {
-  await validateConfiguration(Configuration);
+export const networksFetcher = runWith({
+  memory: '256MB',
+  timeoutSeconds: 540,
+})
+  .pubsub.schedule('* * * * *')
+  .onRun(async () => {
+    await validateConfiguration(Configuration);
 
-  const app = await NestFactory.createApplicationContext(AppModule);
+    const app = await NestFactory.createApplicationContext(AppModule);
 
-  const networksService = [app.get(IVAOService), app.get(VATSIMService)];
+    const networksService = [app.get(IVAOService), app.get(VATSIMService)];
 
-  await Promise.all(
-    networksService.map((service) => service.fetchCurrentLive()),
-  );
-});
+    await Promise.all(
+      networksService.map((service) => service.fetchCurrentLive()),
+    );
+  });
