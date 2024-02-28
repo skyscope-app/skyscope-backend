933aec093019581cbafd69b50c92055a8a0232e1
Luciano Santos <luciano.ssants@gmail.com>
2024-02-28 12:45:27 -0300

    feat(discord): add logger on discord

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,8 +1,9 @@
 import { name, version } from '@/../package.json';
 import { Configuration } from '@/configurations/configuration';
 import { Inject, LoggerService, LogLevel } from '@nestjs/common';
-import { ClsService } from 'nestjs-cls';
 import * as winston from 'winston';
+import { MessageBuilder, Webhook } from 'minimal-discord-webhook-node';
+import { ClsService } from 'nestjs-cls';
 
 export enum Severity {
   DEFAULT = 'DEFAULT',
@@ -29,11 +30,15 @@ export class InternalLogger implements LoggerService {
   private context?: string;
 
   private winston: winston.Logger;
+  private webhook: Webhook;
+  private gcpRegisterLog = `https://console.cloud.google.com/logs/query;query=resource.type%20%3D%20%22cloud_run_revision%22%0Aresource.labels.service_name%20%3D%20%22#SERVICE_NAME#%22%0Aresource.labels.location%20%3D%20%22us-central1%22%0AjsonPayload.requestId%3D%22#requestId#%22%0A%20severity%3E%3DDEFAULT;timeRange=PT5M;cursorTimestamp=#cursorTimestamp#?authuser=1&project=skyscopeapp`;
 
   constructor(
     @Inject(Configuration) private readonly configuration: Configuration,
     private readonly cls: ClsService,
   ) {
+    this.webhook = new Webhook(this.configuration.DISCORD_WEBHOOK_URL);
+
     this.winston = winston.createLogger({
       format: winston.format.json(),
       defaultMeta: { name, version },
@@ -65,7 +70,7 @@ export class InternalLogger implements LoggerService {
     });
   }
 
-  error(message: any, ...optionalParams: any[]) {
+  async error(message: any, ...optionalParams: any[]) {
     if (!this.currentLogLevels.includes('error')) return;
     if (!this.shouldDisplayContext(optionalParams)) return;
 
@@ -75,18 +80,43 @@ export class InternalLogger implements LoggerService {
       requestId = this.cls.getId();
     } catch {}
 
+    console.log(this.cls.get());
+
     let stackTrace;
 
     if (optionalParams.length == 2) {
       stackTrace = optionalParams[0].stack || optionalParams[0];
     }
 
+    const context =
+      optionalParams && optionalParams.length > 0
+        ? optionalParams.pop()
+        : this.context;
+
+    const webhook = new MessageBuilder()
+      .setTitle(`ERROR`)
+      .addField('Context', `${context}`, true)
+      .addField('RequestId', `${requestId}`, true)
+      .addField('Version', `${this.configuration.APP_VERSION}`, true)
+      .addField('StackTrace', `${stackTrace || 'No stack trace'}`, true)
+      .setColor('#ED4337' as any)
+      .setDescription(
+        `This is an error message at ${this.configuration.APP_NAME} service`,
+      )
+      .addField(
+        'log',
+        `[Click here to see log](${this.gcpRegisterLog
+          .replace('#requestId#', requestId!)
+          .replace('#cursorTimestamp#', new Date().toISOString())
+          .replace('#SERVICE_NAME#', this.configuration.SERVICE_NAME)} )`,
+      )
+      .setTimestamp();
+
+    await this.webhook.send(webhook);
+
     this.winston.error({
       message,
-      context:
-        optionalParams && optionalParams.length > 0
-          ? optionalParams.pop()
-          : this.context,
+      context,
       severity: Severity.ERROR,
       time: new Date(),
       requestId,
