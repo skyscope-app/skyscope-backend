2111a0547db49ea4cd129a67f28e20922f9fe919
Luciano Santos <luciano.ssants@gmail.com>
2024-03-06 12:16:54 -0300

    fix(cors): correct domain

--- src/main.ts
+++ src/main.ts
@@ -62,8 +62,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://skyscopeapp.com',
-      'https://www.skyscopeapp.com',
+      'https://skyscope.app',
+      'https://www.skyscope.app',
     ],
   });
   app.setGlobalPrefix('/api/v1');
