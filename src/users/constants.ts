ed06d91cd6eecb15f8c684e309d5507fa6958697
Luciano Santos <luciano.ssants@gmail.com>
2024-04-20 15:34:37 -0300

    fix(user): add more mimetypes as valid profile image

---
+++ src/users/constants.ts
@@ -0,0 +1,12 @@
+export const ValidProfileImageMimeTypes = [
+  'images/apng',
+  'image/avif',
+  'image/gif',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'image/webp',
+  'image/bmp',
+  'image/x-icon',
+  'image/tiff',
+];
