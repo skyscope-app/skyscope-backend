6ea53490641773056371a5a6fc91f658feb6b7a6
Luciano Santos <luciano.ssants@gmail.com>
2024-05-20 01:32:02 -0300

    fix: generate new tag

--- src/networks/services/flights-search.service.ts
+++ src/networks/services/flights-search.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@/networks/dtos/live-flight.dto';
 import { NetworksService } from '@/networks/services/networks.service';
 import { Nullable } from '@/shared/utils/nullable';
+import { searchInObjectRecursive } from '@/shared/utils/object';
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
@@ -18,28 +19,26 @@ export class FlightsSearchService {
   ) {}
 
   async findByParams(term: string): Promise<LiveFlight[]> {
-    // const flights = await this.networksService.fetchLiveFlights();
+    const flights = await this.networksService.fetchLiveFlights();
 
-    // return flights.filter((flight) => {
-    //   return searchInObjectRecursive(flight, term, [
-    //     'flightPlan.arrival.lat',
-    //     'flightPlan.arrival.lng',
-    //     'flightPlan.departure.lng',
-    //     'flightPlan.departure.lat',
-    //     'flightPlan.alternate.lng',
-    //     'flightPlan.alternate.lat',
-    //     'flightPlan.alternate2.lng',
-    //     'flightPlan.alternate2.lat',
-    //     'flightPlan.level',
-    //     'position.lat',
-    //     'position.lng',
-    //     'position.heading',
-    //     'position.altitude',
-    //     'position.groundSpeed',
-    //   ]);
-    // });
-
-    return [];
+    return flights.filter((flight) => {
+      return searchInObjectRecursive(flight, term, [
+        'flightPlan.arrival.lat',
+        'flightPlan.arrival.lng',
+        'flightPlan.departure.lng',
+        'flightPlan.departure.lat',
+        'flightPlan.alternate.lng',
+        'flightPlan.alternate.lat',
+        'flightPlan.alternate2.lng',
+        'flightPlan.alternate2.lat',
+        'flightPlan.level',
+        'position.lat',
+        'position.lng',
+        'position.heading',
+        'position.altitude',
+        'position.groundSpeed',
+      ]);
+    });
   }
 
   async findByID(flightId: string): Promise<Nullable<LiveFlightWithTracks>> {
